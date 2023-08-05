b978686b8c8bf3d7ce748348670de0aab862405e
alexh-ps <alex.holderness@dojo.tech>
2023-08-05 02:38:49 +0100

    update documenation and help section

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Web application to demo Streamlit connections, leveraging the UK's Companies House's free API service.
 
 Submitted in part for consideration in the [Streamlit Hackathon](https://discuss.streamlit.io/t/connections-hackathon/47574)
+
+Guide to use:
+1. Create a free developer account at [Companies House](https://developer.company-information.service.gov.uk/)
+2. Create an application (select live)
+3. Select REST API
+4. Insert public IP (seen in sidebar) into restricted IPs
+5. Copy API key into sidebar input
+6. Start querying! Why not try Facebook or Dominos?
--- demo_page.py
+++ demo_page.py
@@ -3,6 +3,7 @@ from companies_house_connection import CompaniesHouseConnection
 
 import requests
 import pandas as pd
+import socket
 
 st.set_page_config(
     page_title="Companies House Connection Demo",
@@ -17,29 +18,68 @@ st.set_page_config(
 )
 
 def get_lat_lng(postcode):
+    '''
+    Function to get latitude and longitude from a given postcode using the 'postcodes.io' API
+    '''
+    # build url for api call
     base_url = f"http://api.postcodes.io/postcodes/{postcode}"
     response = requests.get(base_url)
     data = response.json()
     if data['status'] == 200:
+        # extract lat and long
         latitude = data['result']['latitude']
         longitude = data['result']['longitude']
         return pd.Series([latitude, longitude])
     else:
         return pd.Series([None, None])
 
+def get_public_ip():
+    '''
+    Function to get the public IP address of the system using the 'httpbin.org' API
+    '''
+    try:
+        response = requests.get("https://httpbin.org/ip")
+        data = response.json()
+        return data["origin"]
+    except requests.RequestException:
+        return None
+
+with st.sidebar:
+    # input field for api key if not in .streamlit/secrets.toml
+    api_key = st.text_input("Companies House API Key")
+    public_ip = get_public_ip()
+    if public_ip:
+        # display public ip to simplify companies house account set-up
+        st.markdown(f"Public IP address: `{public_ip}`")
+# if api key not stored in .streamlit/secrets.toml
+api_key = api_key if api_key else st.secrets["api_key"]
+
+
 with st.form("submit_form"):
     input_company = st.text_input("Enter UK Companies House Search Query")
     with st.expander("Help"):
-        st.write("Why not try Facebook, or Dominos?")
+        st.markdown("""
+       # Companies-House-Explorer
+        Web application to demo Streamlit connections, leveraging the UK's Companies House's free API service.
+
+        Submitted in part for consideration in the [Streamlit Hackathon](https://discuss.streamlit.io/t/connections-hackathon/47574)
+
+        Guide to use:
+        1. Create a free developer account at [Companies House](https://developer.company-information.service.gov.uk/)
+        2. Create an application (select live)
+        3. Select REST API
+        4. Insert public IP (seen in sidebar) into restricted IPs
+        5. Copy API key into sidebar input
+        6. Start querying! Why not try Facebook or Dominos?
+        """)
     submitted = st.form_submit_button("Submit")
 
 # build connection
-conn = st.experimental_connection('companieshouse', type=CompaniesHouseConnection, api_key=st.secrets["api_key"])
+conn = st.experimental_connection('companieshouse', type=CompaniesHouseConnection, api_key=api_key)
 
 if submitted and input_company:
     # search on specified input
     company_data = conn.search_companies(input_company)
-
     # get coordinates from UK postcodes
     company_data[['latitude', 'longitude']] = company_data['address.postal_code'].apply(get_lat_lng)
     # copy coords to df 
